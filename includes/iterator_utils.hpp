70975671ace60c540c45d2b1e15a4adf1a7e9a4f
axaidan <axaidan@hotmail.com>
2022-05-31 13:13:22 +0200

    reverse_iterator - wrote prototypes

--- includes/iterator_utils.hpp
+++ includes/iterator_utils.hpp
@@ -62,6 +62,75 @@ template<class Category,
 			typedef Reference reference;
 		};
 
+template<class Iter>
+class reverse_iterator : public ft::iterator_traits<Iter>
+{
+	public:
+	typedef typename ft::iterator_traits<Iter>::difference_type 	difference_type;
+	typedef typename ft::iterator_traits<Iter>::value_type			value_type;
+	typedef typename ft::iterator_traits<Iter>::pointer				pointer;
+	typedef typename ft::iterator_traits<Iter>::reference			reference;
+	typedef typename ft::iterator_traits<Iter>::iterator_category	iterator_category;
+
+	private:
+	Iter	_iterator;
+
+	public:
+	reverse_iterator(void) :
+		_iterator(NULL)
+	{
+	}
+	reverse_iterator(const reverse_iterator/*<Iter>*/ & src) :
+		_iterator(src._iterator)
+	{
+	}
+	explicit reverse_iterator(const Iter & src) :
+		_iterator(src)
+	{
+	}
+	reverse_iterator &	operator=(const revserse_iterator & rhs)
+	{
+		_iterator = src._iterator;
+		return (*this);
+	}
+	Iter	base(void) const
+	{
+		return (_iterator);
+	}
+	reference			operator*(void) const
+	{
+		Iter	tmp(_iterator);
+		tmp--;
+		return (*tmp);
+	}
+	pointer				operator->(void) const
+	{
+		return (&(operator*()));
+	}
+	reverse_iterator	operator++(void);
+	reverse_iterator &	operator++(int);
+	reverse_iterator	operator--(void);
+	reverse_iterator &	operator--(int);
+	reverse_iterator	operator+=(void);
+	reverse_iterator	operator-=(void);
+	reverse_iterator	operator+(difference_type n);
+	reverse_iterator	operator-(difference_type n);
+	difference_type		operator-(const reverse_iterator & rhs) const;
+
+	bool	operator==(const reverse_iterator & rhs) const;
+	bool	operator!=(const reverse_iterator & rhs) const;
+	bool	operator<(const reverse_iterator & rhs) const;
+	bool	operator>(const reverse_iterator & rhs) const;
+	bool	operator<=(const reverse_iterator & rhs) const;
+	bool	operator>=(const reverse_iterator & rhs) const;
+
+	reference	operator[](difference_type n) const;
+	
+
+
+	
+};
+
 
 
 } // namespace ft
